841a4acf65f952bc3191cb8ab98a6148d058819a
David Borzek <mail@davidborzek.de>
2021-12-01 16:46:07 +0100

    feat: add backend client with search method

---
+++ packages/frontend/src/clients/api.ts
@@ -0,0 +1,25 @@
+import { SearchResponse } from './api.types';
+import axios from 'axios';
+
+type SearchOptions = {
+  limit: number;
+  type: ('album' | 'artist' | 'track')[];
+};
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
+export default class ApiClient {
+  public static search = async (
+    query: string,
+    options?: SearchOptions
+  ): Promise<SearchResponse> => {
+    const { data } = await axios.get<SearchResponse>(`${API_URL}/api/search`, {
+      params: {
+        query,
+        ...options,
+      },
+    });
+
+    return data;
+  };
+}
---
+++ packages/frontend/src/clients/api.types.ts
@@ -0,0 +1,26 @@
+export type Artist = {
+  id: string;
+  name: string;
+  imageUrl?: string;
+};
+
+export type Album = {
+  id: string;
+  name: string;
+  artists: Artist[];
+  imageUrl?: string;
+};
+
+export type Track = {
+  id: string;
+  name: string;
+  artists: Artist[];
+  album: Album;
+  imageUrl?: string;
+};
+
+export type SearchResponse = {
+  artists: Artist[];
+  albums: Album[];
+  tracks: Track[];
+};
