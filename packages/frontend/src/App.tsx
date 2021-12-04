2dd643dfb8d64e53c55713b3f071bb7966a05291
David Borzek <mail@davidborzek.de>
2021-12-04 11:55:10 +0100

    feat: include playlist view in router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React, { ReactElement } from 'react';
 
 import Player from './views/Player/Player';
+import PlaylistView from './views/PlaylistView/PlaylistView';
 import { Search } from './views/Search/Search';
 
 export default function App(): ReactElement {
@@ -12,6 +13,7 @@ export default function App(): ReactElement {
       <Routes>
         <Route path="/" element={<Player />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/playlists" element={<PlaylistView />} />
       </Routes>
     </BrowserRouter>
   );
