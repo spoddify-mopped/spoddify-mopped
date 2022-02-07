4259bcd919c8b804edf857d050d185ddfc835441
David Borzek <mail@davidborzek.de>
2022-02-07 17:59:19 +0100

    refactor (frontend): use scss for playlists view

--- packages/frontend/src/views/PlaylistsView/PlaylistView.tsx
+++ packages/frontend/src/views/PlaylistsView/PlaylistView.tsx
@@ -1,11 +1,10 @@
-import './PlaylistView.css';
-
 import React, { useEffect, useState } from 'react';
 
 import ApiClient from '../../clients/api';
 import Error from '../../components/Error/Error';
 import { ReactComponent as Play } from '../../resources/play-solid.svg';
 import { Playlist } from '../../clients/api.types';
+import styles from './PlaylistView.module.scss';
 import { useNavigate } from 'react-router-dom';
 
 export default function PlaylistView(): React.ReactElement {
@@ -22,15 +21,15 @@ export default function PlaylistView(): React.ReactElement {
 
   const renderPlaylists = () => {
     return playlists.map((playlist) => (
-      <div className="singlePlaylist" key={playlist.id}>
+      <div className={styles.playlist} key={playlist.id}>
         <p
-          className="playlistName"
+          className={styles.name}
           onClick={() => navigate(`/playlist/${playlist.id}`)}
         >
           {playlist.name}
         </p>
         <button
-          className="button"
+          className={styles.button}
           onClick={() => ApiClient.playPlaylist(playlist.id)}
         >
           <Play />
@@ -43,10 +42,19 @@ export default function PlaylistView(): React.ReactElement {
     return <Error />;
   }
 
+  if (playlists.length === 0) {
+    return (
+      <div className={styles.noContent}>
+        <span>No playlists.</span>
+        <span>Get started and add some tracks.</span>
+      </div>
+    );
+  }
+
   return (
-    <div className="playlist">
-      <h1 className="playlistHeadline">Wähle ein Genre aus</h1>
-      <div className="playlistContainer">{renderPlaylists()}</div>
+    <div className={styles.container}>
+      <h1 className={styles.headline}>Choose a genre</h1>
+      <div className={styles.playlistContainer}>{renderPlaylists()}</div>
     </div>
   );
 }
