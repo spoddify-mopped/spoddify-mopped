2a1a403cd25952e9c80741dd7bf1df47ceeb02c7
David Borzek <mail@davidborzek.de>
2022-02-10 23:59:54 +0100

    refactor (backend): use express-validator in search controller

--- packages/backend/src/controllers/search.ts
+++ packages/backend/src/controllers/search.ts
@@ -1,23 +1,42 @@
+import { matchedData, query } from 'express-validator';
+
+import RequestError from '../error/request';
 import SpotifySearchService from '../services/search';
 import express from 'express';
 
-type SpotifySearchType = 'album' | 'artist' | 'track';
+const searchTypes = ['album', 'artist', 'track'];
 
-const spotifySearchTypeList = ['album', 'artist', 'track'];
-
-const isOfSpotifySearchType = (type: string): type is SpotifySearchType =>
-  spotifySearchTypeList.includes(type);
-
-const isOfSpotifySearchTypeList = (
-  types: string[]
-): types is SpotifySearchType[] => {
+const isType = (type: string): boolean => searchTypes.includes(type);
+const isTypeList = (types: string[]): boolean => {
   for (const type of types) {
-    if (!isOfSpotifySearchType(type)) {
+    if (!isType(type)) {
       return false;
     }
   }
   return true;
 };
+
+type SearchQuery = {
+  query: string;
+  limit?: number;
+  type: string[];
+};
+
+const typeValidator = (type) => {
+  if (typeof type === 'string') {
+    return isType(type);
+  }
+
+  if (Array.isArray(type)) {
+    return isTypeList(type);
+  }
+};
+
+const searchValidationChain = [
+  query('query').isString(),
+  query('limit').optional().isNumeric().toInt(),
+  query('type').custom(typeValidator).toArray(),
+];
 
 export default class SearchController {
   public path = '/search';
@@ -32,34 +51,26 @@ export default class SearchController {
   }
 
   public initializeRoutes(): void {
-    this.router.get(`${this.path}`, this.search);
+    this.router.get(`${this.path}`, searchValidationChain, this.search);
   }
 
   private search = async (
     request: express.Request,
-    response: express.Response
+    response: express.Response,
+    next: express.NextFunction
   ): Promise<void> => {
-    const query = request.query['query'] as string;
-    let type = request.query['type'] as string | string[];
-    const limit =
-      Number.parseInt(request.query['limit'] as string) || undefined;
-
-    if (!Array.isArray(type)) {
-      type = [type];
-    }
-
-    if (!isOfSpotifySearchTypeList(type)) {
-      response.sendStatus(400);
+    const validationError = RequestError.validationResult(request);
+    if (validationError) {
+      next(validationError);
       return;
     }
 
-    if (!query) {
-      response.sendStatus(400);
-      return;
-    }
+    const data = matchedData(request, {
+      includeOptionals: true,
+    }) as SearchQuery;
 
     await this.spotifySearchService
-      .search(query, type, limit)
+      .search(data.query, data.type, data.limit)
       .then((result) => {
         response.send(result);
       })
