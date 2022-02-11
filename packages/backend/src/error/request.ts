5f1d14124f40953a9a42be3b2584b6aba7037afb
David Borzek <mail@davidborzek.de>
2022-02-11 18:26:17 +0100

    feat (backend): create request error from spotify api error

--- packages/backend/src/error/request.ts
+++ packages/backend/src/error/request.ts
@@ -1,4 +1,6 @@
-import { StatusCodes } from 'http-status-codes';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+
+import { SpotifyApiError } from '../clients/spotify/error';
 import express from 'express';
 import { validationResult } from 'express-validator';
 
@@ -31,6 +33,20 @@ export default class RequestError extends Error {
           .map((err) => `${err.msg} for parameter '${err.param}'`)[0],
         StatusCodes.BAD_REQUEST
       );
+    }
+  };
+
+  public static fromSpotifyApiError = (err: SpotifyApiError): RequestError => {
+    switch (err.status) {
+      case StatusCodes.BAD_REQUEST:
+      case StatusCodes.NOT_FOUND: {
+        return new RequestError(err.message, err.status);
+      }
+      default:
+        return new RequestError(
+          ReasonPhrases.INTERNAL_SERVER_ERROR,
+          StatusCodes.INTERNAL_SERVER_ERROR
+        );
     }
   };
 }
